f60dbca08e30ff35f0a244a964b0bd1d5b06307c
Rajnishpal003 <Rajnishpal006@gmail.com>
2024-09-13 23:26:15 +0530

    initial

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors'); 
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
 
-mongoose.connect('mongodb://localhost/smartwater');
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb+srv://raj345pal:M3RPRiJ9URYLe2rf@cluster0.t75jj.mongodb.net/?retryWrites=true&w=majority&appName=cluster0');
 
 const waterSchema = new mongoose.Schema({
   userId: String,
